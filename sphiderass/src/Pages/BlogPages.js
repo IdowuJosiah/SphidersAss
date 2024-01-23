b1b0d2d9da574ee3c6a29c32968f8b2046612ba2
Idowu Josiah <139690428+IdowuJosiah@users.noreply.github.com>
2024-01-23 18:59:21 +0100

    latest update

--- sphiderass/src/Pages/BlogPages.js
+++ sphiderass/src/Pages/BlogPages.js
@@ -9,36 +9,42 @@ const BlogPages = () => {
             blogHeader: "Stay current with all the latest trends, software, industry expectation and more.",
             blogPreviewText:"Ensuring our online presence is protected is important in the electronic age we live in today since digital connections are essentialto our daily lives. The following looks at some basic safety tips for operating on the internet",
             blogLink : "/article-one",
+            blogImage:"/bunfd.png",
         },
         {
             date: " 11th January 2024",
             blogHeader: "Web Development A Beginners Guide to Web Development",
             blogPreviewText: "Ensuring our online presence is protected is important in the electronic age we live in today since digital connections are essential to our daily lives. The following looks at some basic safety tips for operating on the internet",
             blogLink: "/article-two",
+            blogImage: "/techblog-four.jpeg"
         },
         {
             date: "17th January 2024.",
             blogHeader: "Tech Trends Shaping the Future of Business.",
             blogPreviewText: "                               Organizations are using AI and machine learning on a daily basis for automation, data analysis, and decision-making.",
             blogLink: "/article-three",
+            blogImage: "/techblog-five.jpeg",
         },
         {
             date: "14th January 2024.",
             blogHeader: "Web Development: A Beginners Guide to Web Development",
             blogPreviewText: "Starting a web development career as a beginner can be thrilling and challenging as well.  Here is a guide to support you in knowing the foundations...",
             blogLink: "/article-four",
+            blogImage: "/techblog-one.jpeg"
         },
         {
             date: "16th January 2024.",
             blogHeader: "THE INTRODUCTION OF ARTIFICIAL INTELLIGENCE AND MACHINE LEARNING",
             blogPreviewText: "Organizations are using AI and machine learnin g on a daily basis for automation, data analysis, and decision-making.",
             blogLink: "/article-five",
+            blogImage: "/techblog-three.jpeg",
         },
         {
             date: "13th January 2024",
             blogHeader: "Web Development Trends in Web Development to Look Out for in 2024",
             blogPreviewText: "Web development is the process of creating, building, and coding websites with an array of tools and programming languages.",
             blogLink: "article-six",
+            blogImage: "/techblog-three.jpeg",
         },
     ]
     return(
@@ -61,22 +67,21 @@ const BlogPages = () => {
                        return(
                            <div className="bpb-box-div">
                                <p className="bpb-box-preview-image">
-                                   <img className="blog-page-blogs-image" src="/bunfd.png"/>
+                                   <img className="blog-page-blogs-image" src={previewDetails.blogImage}/>
                                </p>
                                <div>
                                    <p className="bpb-time-stamp">
                                        {previewDetails.date}
                                    </p>
                                    <h5>
-                                       Introduction to Cybersecurity: Safeguarding Your Digital Environment
-
+                                       {previewDetails.blogHeader}
                                    </h5>
                                    <p>
-                                       Ensuring our online presence is protected is important in the electronic age we live in today since digital connections are essential to our daily lives. The following looks at some basic safety tips for operating on the internet,
+                                       {previewDetails.blogPreviewText}
                                    </p>
-                                   <p>
-
-                                   </p>
+                                   <a href={previewDetails.blogLink}>
+                                        read more
+                                   </a>
                                </div>
 
                            </div>
